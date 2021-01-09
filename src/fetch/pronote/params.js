c5000356adfc3c13c54be63fe011e15933248971
Androz2091 <androz2091@gmail.com>
2021-01-09 12:00:21 +0100

    Bon ben ouais ya pas de general

--- src/fetch/pronote/params.js
+++ src/fetch/pronote/params.js
@@ -12,11 +12,6 @@ async function getParams(session)
         donnees: { Uuid: getUUID(session, session.aesIV) }
     });
 
-    const general = params.General;
-    if (!general) {
-        console.log(chalk.red('PUTAIN YA PAS DE GENERAL ????'), params)
-        return;
-    }
     return {
         navigatorId: params.identifiantNav,
         fonts: parse(params.listePolices, false).map(o => o.L),
